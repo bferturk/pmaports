2018571e895af5ff251c09e2264aa39547942758
Pierre Parent <pierre.parent@pparent.fr>
2018-09-23 12:45:26 +0200

    Qtwebbrowser initial port (pparent76's version)

---
+++ main/qtwebbrowser/startup-qtwebbrowser.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+rm /tmp/qt-WebBrowser-last-url.txt
+
+if [ ! -f  "/usr/bin/libEGL.so" ]; then  
+        sudo ln -s /usr/bin/libEGL.so /usr/lib/libEGL.so                                          
+fi
+
+if [ ! -f  "/usr/bin/libGLESv2.so" ]; then  
+        sudo ln -s /usr/bin/libGLESv2.so /usr/lib/libGLESv2.so                                          
+fi
+
+crash_at_start=0
+need_to_start=1
+
+while [ "$need_to_start" -eq "1" ]; do
+
+	START=$(date +%s)
+	#Start the app with X11 and scale factor to 1.75
+	QT_SCALE_FACTOR=1.75 QT_QPA_PLATFORM=xcb qtwebbrowser-bin >/tmp/qtwebbrowserlog 2>&1 
+	END=$(date +%s)
+	EXEC_TIME=$(( $END - $START ))
+
+	#See if it crashed at startup or not
+	if [ "$EXEC_TIME" -lt "17" ]; then
+		crash_at_start=$(( crash_at_start + 1 ))
+	else
+		crash_at_start=0;
+	fi
+
+	#If the browser did not crash we do not need to restart it
+	#It was closed intentionally by the user
+	#If it crashed 4 times in a row at startup don't try again 
+	#in order to avoid loops
+	cat /tmp/qtwebbrowserlog | grep "Received signal"
+	if [ "$?" -ne "0" ] || [ "$crash_at_start" -ge "4" ]; then
+		need_to_start=0;
+	fi
+done
