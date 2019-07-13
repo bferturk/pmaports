74c37d017c02e31ea3f1fd1695b82707b1986997
Oliver Smith <ollieparanoid@bitmessage.ch>
2019-07-13 16:09:47 +0200

    .sr.ht/submit.py: update for bpo rewrite
    Read all variables from the environment instead of using argparse. This
    allows setting different variables for different service backends, we
    will at least support a "local" backend for local testing, and a
    sourcehut one for running jobs on sr.ht. The code for these services
    simply sets the appropriate environment variable, before calling the
    common code that will eventually call submit.py.
    
    Adjust HTTP header variables, they are now called X-BPO-Token,
    X-BPO-Arch etc.
    
    Instead of sending a commit, send the current "push-id". That ID is
    assigned by the bpo server when the gitlab push hook gets executed,
    so it is always about a specific push. Previously we had mixed such
    pushes with commits together and this wasn't very intuitive as commits
    then meant sometimes a specific commit, but sometimes a whole push of
    possibly multiple commits.
    
    Use " instead of ' for strings to increase consistency with the
    pmbootstrap code. Consistently talk about "token", not sometimes token,
    sometimes secret.

--- .sr.ht/submit.py
+++ .sr.ht/submit.py
@@ -1,74 +1,78 @@
 #!/usr/bin/env python3
-import os
 import argparse
 import json
-
+import os
 import requests
 
-commit_default = os.environ['COMMIT'] if 'COMMIT' in os.environ else None
+# Require environment vars
+for key in ["BPO_API_ENDPOINT",
+            "BPO_API_HOST",
+            "BPO_ARCH",
+            "BPO_TOKEN_FILE",
+            "BPO_PAYLOAD_FILES",    # space separated list of files
+            "BPO_PAYLOAD_IS_JSON",  # set to "1" to enable
+            "BPO_PUSH_ID",
+            ]:
+    if not key in os.environ:
+        print("ERROR: missing environment variable: " + key)
+        exit(1)
 
-parser = argparse.ArgumentParser(description='tool to send data to build.postmarketos.org')
-parser.add_argument('--token', default='~/.pmos_token', help='secret token to authenticate to build.postmarketos.org')
-parser.add_argument('--host', default='https://build.postmarketos.org', help='base url for the submit request')
-parser.add_argument('--commit', default=commit_default, help='value for the X-Commit header')
-parser.add_argument('--arch', default='x86_64', help='value for the X-Arch header')
-parser.add_argument('--id', help='id of the job in the queue, value for the X-Id header')
-parser.add_argument('--json', action='store_true', help='datafile is a json file, do extra sanity checks')
-parser.add_argument('--verbose', '-v', action='store_true', help='show more debug info')
-parser.add_argument('endpoint', help='endpoint name on the API')
-parser.add_argument('datafile', help='file containing the data to be submitted', nargs='+')
-args = parser.parse_args()
+# Parse and check files
+files = os.environ["BPO_PAYLOAD_FILES"].split(" ")
+for path in files:
+    if not os.path.exists(path):
+        print("ERROR: file not found: " + path)
+        exit(1)
 
-if args.commit is None:
-    print('You need to either add COMMIT to the environment or specify --commit')
-    exit(1)
+# Load token
+with open(os.path.expanduser(os.environ["BPO_TOKEN_FILE"]),
+          encoding="utf-8") as handle:
+    token = handle.read().strip()
 
-if args.verbose:
-    print('Environment:')
-    print(os.environ)
+# Load other env vars
+url = (os.environ["BPO_API_HOST"] + "/api/job-callback/" +
+       os.environ["BPO_API_ENDPOINT"])
+arch = os.environ["BPO_ARCH"]
+is_json = (os.environ["BPO_PAYLOAD_IS_JSON"] == "1")
+push_id = os.environ["BPO_PUSH_ID"]
 
-with open(os.path.expanduser(args.token), encoding="utf-8") as handle:
-    secret = handle.read().strip()
-
-url = '{}/api/{}'.format(args.host, args.endpoint)
-
-if args.json:
-    if len(args.datafile) > 1:
-        print("json mode doesn't support multiple input files")
+# Submit JSON
+if is_json:
+    if len(files) > 1:
+        print("ERROR: json mode doesn't support multiple input files")
         exit(1)
 
     # Send contents of file as HTTP POST with json payload
-    with open(args.datafile[0], encoding="utf-8") as handle:
+    with open(files[0], encoding="utf-8") as handle:
         data = handle.read()
     data = json.loads(data)
 
-    print('Sending json data to {}'.format(url))
+    print("Sending JSON to: " + url)
     response = requests.post(url, json=data, headers={
-        'X-Secret': secret,
-        'X-Commit': args.commit,
-        'X-Arch': args.arch
+        "X-BPO-Arch": arch,
+        "X-BPO-Push-Id": push_id,
+        "X-BPO-Token": token,
     })
-else:
-    files = []
-
-    for file in args.datafile:
-        filename = os.path.basename(file)
+else:  # Submit blobs
+    for path in files:
+        filename = os.path.basename(path)
         # Send contents of file as HTTP POST with multipart/formdata payload
-        files.append(('file[]', (filename, open(file, 'rb'), 'application/octet-stream')))
+        print("Appending: " + path)
+        files.append(("file[]", (filename,
+                                 open(path, "rb"),
+                                 "application/octet-stream")))
 
-        print('Uploading {} to {}'.format(filename, url))
+    print("Uploading to: " +  url)
 
     response = requests.post(url, files=files, headers={
-        'X-Secret': secret,
-        'X-Commit': args.commit,
-        'X-Arch': args.arch,
-        'X-Id': args.id
+        "X-BPO-Arch": arch,
+        "X-BPO-Push-Id": push_id,
+        "X-BPO-Token": token,
     })
 
 if response.status_code > 399:
-    print('Error occurred:')
+    print("Error occurred:")
     print(response.content.decode())
     exit(1)
-
 else:
     print(response.content.decode())
